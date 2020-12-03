e09d682f24640de182865441d839834cd972a513
ErinCain <ecain@flowwest.com>
2020-12-03 09:28:43 -0800

    fixed typo in fahreinheit to celcius function

--- R/temperature_converters.R
+++ R/temperature_converters.R
@@ -5,12 +5,13 @@
 #' @param temp_fahrenheit A temperature in fahrenheit
 #' @return Returns the value of the temperature in Celcius
 #' @examples
+#' Pass a temperature in fahrenheit in and it will return that temperature in celcius
 #' fahrenheit_to_celcius(5)
 #'
 #'
 # Temperature Conversion function from fahrenheit to celcius
 
-fahrenheitt_to_celcius <- function(temp_fahrenheit) {
+fahrenheit_to_celcius <- function(temp_fahrenheit) {
   temp_celcius <- ((temp_fahrenheit - 32) * (5/9))
   return(temp_celcius)
 }
